6bcbe7e5a035e36923f14dc369ed0858b822b723
Outhman Moumou <tangerino2011@gmail.com>
2024-12-29 08:35:55 +0100

    Main component responsible for managing states by useReducer hook and also responsible for saving/retrieving  the data from localStorage

--- src/components/BudgetTracker.jsx
+++ src/components/BudgetTracker.jsx
@@ -1,18 +1,72 @@
-import React from "react";
-import { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Header } from "./Header";
 import { BudgetLimitSection } from "./BudgetLimitSection";
 import { IncomeSection } from "./IncomeSection";
 import { ExpensesSection } from "./ExpensesSection";
 import { SpendingAnalysis } from "./SpendingAnalysis";
 const BudgetTracker = () => {
+  const initialState = {
+    expenses: [],
+    incomes: [],
+    budgetLimit: null,
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_EXPENSE":
+        return {
+          ...state,
+          expenses: [...state.expenses, action.payload],
+        };
+      case "ADD_INCOME":
+        return {
+          ...state,
+          incomes: [...state.incomes, action.payload],
+        };
+      case "REMOVE_EXPENSE":
+        return {
+          ...state,
+          expenses: state.expenses.filter(
+            (expense) => expense.id !== action.payload
+          ),
+        };
+      case "REMOVE_INCOME":
+        return {
+          ...state,
+          incomes: state.incomes.filter(
+            (income) => income.id !== action.payload
+          ),
+        };
+      case "SET_BUDGET_LIMIT":
+        return {
+          ...state,
+          budgetLimit: action.payload,
+        };
+      case "RESET_BUDGET_LIMIT":
+        return {
+          ...state,
+          budgetLimit: null,
+        };
+      case "RESET_ALL":
+        return initialState;
+      default:
+        return state;
+    }
+  };
+  const loadState = () => {
+    const savedState = localStorage.getItem("budgetTrackerState");
+    return savedState ? JSON.parse(savedState) : initialState;
+  };
+  const [state, dispatch] = useReducer(reducer, loadState());
+  useEffect(() => {
+    localStorage.setItem("budgetTrackerState", JSON.stringify(state));
+  }, [state]);
   return (
     <div className="min-h-screen bg-gray-50 p-8">
       <div className="max-w-4xl mx-auto space-y-6">
-        <Header />
-        <BudgetLimitSection />
-        <IncomeSection />
-        <ExpensesSection />
+        <Header dispatch={dispatch} items={state} />
+        <BudgetLimitSection dispatch={dispatch} />
+        <IncomeSection dispatch={dispatch} incomesItems={state.incomes} />
+        <ExpensesSection dispatch={dispatch} expenseItems={state.expenses} />
         <SpendingAnalysis />
       </div>
     </div>
