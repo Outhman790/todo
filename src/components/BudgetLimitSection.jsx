214c41ddb987734729d226df11501eeef3636f90
Outhman Moumou <tangerino2011@gmail.com>
2024-12-29 10:14:04 +0100

    user can add budget limit

--- src/components/BudgetLimitSection.jsx
+++ src/components/BudgetLimitSection.jsx
@@ -1,22 +1,35 @@
-// src/components/BudgetLimitSection.jsx
-import React from "react";
+import React, { useState } from "react";
 import { DollarSign } from "lucide-react";
 
-export const BudgetLimitSection = () => (
-  <div className="bg-white rounded-lg shadow p-6">
-    <div className="flex items-center justify-between mb-4">
-      <h2 className="text-xl font-semibold text-gray-800">Budget Limit</h2>
-      <div className="flex items-center space-x-2">
-        <DollarSign className="text-gray-500" size={20} />
-        <input
-          type="number"
-          className="border rounded-lg px-3 py-2 w-32"
-          placeholder="Set limit"
-        />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
-          Set
-        </button>
+export const BudgetLimitSection = ({ dispatch, budgetlimit }) => {
+  const [budgetLimit, setBudgetLimit] = useState("");
+  return (
+    <div className="bg-white rounded-lg shadow p-6">
+      <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center gap-2">
+          <h2 className="text-xl font-semibold text-gray-800">Budget Limit</h2>
+          <p className="text-red-500 text-xl font-bold">${budgetlimit}</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <DollarSign className="text-gray-500" size={20} />
+          <input
+            value={budgetLimit}
+            onChange={(e) => setBudgetLimit(e.target.value)}
+            type="number"
+            className="border rounded-lg px-3 py-2 w-32"
+            placeholder="Set limit"
+          />
+          <button
+            onClick={(e) => {
+              dispatch({ type: "SET_BUDGET_LIMIT", payload: budgetLimit });
+              setBudgetLimit("");
+            }}
+            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          >
+            Set
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
